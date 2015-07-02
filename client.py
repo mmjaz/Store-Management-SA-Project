b9efb43ce2f64d916ba069fa2e85ab3cad973abb
Mehdi Jaz <mmjaziryan@gmail.com>
2015-07-02 23:14:35 +0430

    Update client.py
    Final

--- client.py
+++ client.py
@@ -1,39 +1,32 @@
-import SocketServer,socket
+import SocketServer,socket,ast
 
-ip="127.0.0.1"
-port=9999
-
-def client(ip, port, message):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((ip, port))
-    try:
-        sock.sendall(bytes(message))
-        response = str(sock.recv(1024))
-        print("Received: {}".format(response))
-    finally:
-        sock.close()
-
-client(ip,port,"[[600,'abc','c',5],1]")
-'''
-
-
-while 1:
+#ip="127.0.0.1"
+#port=9999
+#setIP("127.0.0.1",9999)
+class Client():
+    def __init__(self):
+        self.ip="127.0.0.1"
+        self.port=9999
+        self.res=""
+    def setIP(self,ip,port):
+        self.ip=ip
+        self.port=port
     
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    def send(self,message):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((self.ip, self.port))
+        self.sock.sendall(bytes(message))
+        self.res = str(self.sock.recv(1024))
+        self.sock.close()
+    def get(self):
+        if self.res[0]=="[":
+            return ast.literal_eval(self.res)
+        else:
+            return self.res
+        
+y=Client()
+y.send([[""]])
 
-    sock.connect((ip, port))
-    message = raw_input("press : ")
-    if message=="":
-	    sock.close()
-    sock.sendall(bytes(message))
-    response = str(sock.recv(1024))
-    print("Received: {}".format(response))
-    sock.close()
-
-'''
-	
-'''try:
-    sock.sendall(bytes(message))
-    response = str(sock.recv(1024))
-    print("Received: {}".format(response))'''
+print type(y.get()[0])
+print y.get()
